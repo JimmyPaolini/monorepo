ca9b322bbe12abdf22ac8234b9f33255531fcaa8
Jimmy Paolini <jimmy.paolini@shiftsmart.com>
2026-01-26 21:07:11 -0500

    chore(monorepo): 🔧 update commitlint rules for clarity

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -6,29 +6,31 @@ const configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
   plugins: ["commitlint-plugin-gitmoji", "commitlint-plugin-tense"],
   rules: {
-    // ❗
+    // ❗ Breaking change
     "subject-exclamation-mark": [0],
+
     // 😀 Enforce gitmoji at start of commit message
     "start-with-gitmoji": [2, "always"],
 
-    // 💬 Enforce present imperative verb tense
+    // 💬 Enforce grammatical tense
     "tense/subject-tense": [
       2,
       "always",
       { allowedTenses: ["present-imperative"] },
     ],
 
-    // 🏷️ Type enforcement
+    // 🏷️ Enforce enums
     "type-enum": [2, "always", [...(types as string[])]],
-
-    // 🔭 Scope enforcement
     "scope-enum": [2, "always", [...(scopes as string[])]],
 
-    // 📏 Length limits
+    // 📏 Limit lengths
     "header-max-length": [2, "always", 100],
     "body-max-length": [1, "always", 1000],
 
-    // 🔡 Case enforcement
+    // 🦶 Forbid footer
+    "footer-empty": [2, "always"],
+
+    // 🔡 Enforce case
     "type-case": [2, "always", "lower-case"],
     "scope-case": [2, "always", "lower-case"],
     "subject-case": [2, "always", "lower-case"],
@@ -37,7 +39,6 @@ const configuration: UserConfig = {
     "subject-full-stop": [2, "never", "."],
     "subject-empty": [2, "never"],
     "body-leading-blank": [2, "always"],
-    "footer-leading-blank": [2, "always"],
   },
 };
 
