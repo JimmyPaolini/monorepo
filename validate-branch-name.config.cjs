a7458e6c84065786b53a67fd2c1b99123a6cc534
Jimmy Paolini <jimmypaolini@gmail.com>
2026-05-24 22:08:56 +0200

    chore(monorepo): 🔧 add copilot to automated branch name prefixes

--- validate-branch-name.config.cjs
+++ validate-branch-name.config.cjs
@@ -10,7 +10,7 @@
 const { scopes, types } = require("./configuration/conventional.config.cjs");
 
 const specialBranches = ["main"];
-const automatedPrefixes = ["renovate", "dependabot"];
+const automatedPrefixes = ["copilot", "dependabot", "renovate"];
 
 // Build regex pattern
 const typePattern = types.map((type) => type.name).join("|");
