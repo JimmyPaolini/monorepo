c5062647773efbf0fd19d21fb126c5bb1cb6590a
Jimmy Paolini <jimmypaolini@gmail.com>
2026-06-25 16:37:56 -0400

    chore(monorepo): 🔧 update fallow audit and dependency configuration

--- packages/lexico-entities/testing/setup.ts
+++ packages/lexico-entities/testing/setup.ts
@@ -1,7 +1,6 @@
 import fs from "node:fs";
 
 import "reflect-metadata";
-import { noop } from "lodash";
 import { beforeAll, beforeEach, vi } from "vitest";
 
 /**
@@ -20,7 +19,7 @@ beforeAll(() => {
 // 🤐 Suppress console output in tests by default
 beforeEach(() => {
   vi.clearAllMocks();
-  vi.spyOn(console, "log").mockImplementation(noop);
-  vi.spyOn(console, "error").mockImplementation(noop);
-  vi.spyOn(console, "warn").mockImplementation(noop);
+  vi.spyOn(console, "log").mockImplementation(() => {});
+  vi.spyOn(console, "error").mockImplementation(() => {});
+  vi.spyOn(console, "warn").mockImplementation(() => {});
 });
